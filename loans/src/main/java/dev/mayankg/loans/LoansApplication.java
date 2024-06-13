9b79e113dba2c773afd70a04e33a98d2c046b397
Mayank Gupta <ce.mayank8@gmail.com>
2024-06-14 01:24:03 +0530

    finVista-nexus: loans microservice

---
+++ loans/src/main/java/dev/mayankg/loans/LoansApplication.java
@@ -0,0 +1,48 @@
+package dev.mayankg.loans;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * @author Mayank Gupta
+ * @Link: <a href=http://localhost:8090/swagger-ui/index.html>Swagger for Loans Microservice</a>
+ */
+@OpenAPIDefinition(
+        info = @Info(
+                title = "FinVista Nexus's Loans microservice REST API Documentation",
+                description = "FinVista Nexus - Loans microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Mayank Gupta",
+                        email = "techno.sorcerer3@gmail.com",
+                        url = "https://github.com/MayankGupta-dev08"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.thetechnosorcerers.com"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "FinVista Nexus - Loans microservice REST API Documentation",
+                url = "https://www.finvista-nexus.com/swagger-ui.html"
+        ),
+        servers = @Server(
+                description = "Loans server"
+        )
+)
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+public class LoansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoansApplication.class, args);
+    }
+
+}
