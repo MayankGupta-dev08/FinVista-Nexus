2418b52ad20b357bd4fa6edcfcc197c55028568a
Mayank Gupta <ce.mayank8@gmail.com>
2024-06-22 20:23:51 +0530

    finVista-nexus: added gateway server

--- gatewayserver/src/main/java/dev/mayankg/gatewayserver/GatewayServerApplication.java
+++ gatewayserver/src/main/java/dev/mayankg/gatewayserver/GatewayServerApplication.java
@@ -2,7 +2,11 @@ package dev.mayankg.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
+@SuppressWarnings("unused")
 @SpringBootApplication
 public class GatewayServerApplication {
 
@@ -10,4 +14,21 @@ public class GatewayServerApplication {
         SpringApplication.run(GatewayServerApplication.class, args);
     }
 
+    @Bean
+    public RouteLocator fvnBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(p -> p
+                        .path("/fvnbank/accounts/**")
+                        .filters(f -> f.rewritePath("/fvnbank/accounts/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://ACCOUNTS"))
+                .route(p -> p
+                        .path("/fvnbank/loans/**")
+                        .filters(f -> f.rewritePath("/fvnbank/loans/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://LOANS"))
+                .route(p -> p
+                        .path("/fvnbank/cards/**")
+                        .filters(f -> f.rewritePath("/fvnbank/cards/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://CARDS"))
+                .build();
+    }
 }
