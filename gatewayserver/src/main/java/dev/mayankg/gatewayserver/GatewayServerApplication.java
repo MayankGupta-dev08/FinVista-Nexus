91b62eadfe1813ae45dbc524e1146c0172af8e2b
Mayank Gupta <ce.mayank8@gmail.com>
2024-06-24 22:04:29 +0530

    finVista-nexus: retry pattern at gateway for loans ms

--- gatewayserver/src/main/java/dev/mayankg/gatewayserver/GatewayServerApplication.java
+++ gatewayserver/src/main/java/dev/mayankg/gatewayserver/GatewayServerApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @SuppressWarnings("unused")
@@ -34,6 +36,9 @@ public class GatewayServerApplication {
                         .filters(f -> f
                                 .rewritePath("/fvnbank/loans/(?<segment>.*)", "/${segment}")
                                 .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .retry(retryConfig -> retryConfig.setRetries(3).setMethods(HttpMethod.GET)
+                                        .setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true)
+                                )
                         )
                         .uri("lb://LOANS"))
                 .route(p -> p
