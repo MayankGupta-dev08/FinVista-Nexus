04195146357a31ec22ed53b181c9d3b936e30ee2
Mayank Gupta <ce.mayank8@gmail.com>
2024-07-06 19:17:59 +0530

    fvn_v4.0.0: authorization in gateway server

---
+++ gatewayserver/src/main/java/dev/mayankg/gatewayserver/config/KeycloakRoleConverter.java
@@ -0,0 +1,34 @@
+package dev.mayankg.gatewayserver.config;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class KeycloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+    /**
+     * Convert the source object of type {@code S} to target type {@code T}.
+     *
+     * @param source the source object to convert, which must be an instance of {@code S} (never {@code null})
+     * @return the converted object, which must be an instance of {@code T} (potentially {@code null})
+     * @throws IllegalArgumentException if the source cannot be converted to the desired target type
+     */
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt source) {
+        Map<String, Object> realmAccess = (Map<String, Object>) source.getClaims().get("realm_access");
+
+        if (realmAccess == null || realmAccess.isEmpty())
+            return List.of();
+
+        List<String> roles = (List<String>) realmAccess.get("roles");
+        List<GrantedAuthority> grantedAuthorities = roles.stream().map(role -> "ROLE_" + role)
+                .map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+        return grantedAuthorities;
+    }
+}
